ac9a50765bccf2ee57ab005078f8bd7bfffd804c
Jeremy Watt <jermwatt@gmail.com>
2017-08-31 11:20:45 -0500

    test

--- mlrefined_libraries/calculus_library/taylor2d_viz.py
+++ mlrefined_libraries/calculus_library/taylor2d_viz.py
@@ -33,16 +33,16 @@ class visualizer:
             num_frames = kwargs['num_frames']
             
         # initialize figure
-        fig = plt.figure(figsize = (16,8))
+        fig = plt.figure(figsize = (10,5))
         artist = fig
 
         # create subplot with 3 panels, plot input function in center plot
-        gs = gridspec.GridSpec(1, 3, width_ratios=[1,2, 1]) 
+        gs = gridspec.GridSpec(1, 3, width_ratios=[1,3, 1]) 
         ax1 = plt.subplot(gs[0]); ax1.axis('off');
         ax3 = plt.subplot(gs[2]); ax3.axis('off');
 
         # plot input function
-        ax = plt.subplot(gs[1])
+        ax = plt.subplot(gs[1],aspect = 'equal')
         
         max_val = 2.5
         if 'max_val' in kwargs:
@@ -83,14 +83,18 @@ class visualizer:
             g_val = self.g(w_val)
 
             # plot original function
-            ax.plot(w_plot,g_plot,color = 'k',zorder = 0,linewidth=3)                           # plot function
+            ax.plot(w_plot,g_plot,color = 'k',zorder = 0,linewidth=1)                           # plot function
             
             # plot the input/output tangency point
-            ax.scatter(w_val,g_val,s = 120,c = 'lime',edgecolor = 'k',linewidth = 0.7,zorder = 3)            # plot point of tangency
-            
+            ax.scatter(w_val,g_val,s = 80,c = 'red',edgecolor = 'k',linewidth = 1,zorder = 3,marker = 'X')            # plot point of tangency
+            ax.scatter(w_val,0,s = 100,c = 'red',edgecolor = 'k',linewidth = 1,zorder = 3)            # plot point of tangency
+            tempy = np.linspace(0,g_val,100)
+            tempx = w_val*np.ones((100))
+            ax.plot(tempx,tempy,linewidth = 0.7,color = 'k',linestyle = '--',zorder = 1)
+                
             # label axes
-            ax.set_xlabel('$w$',fontsize = 25)
-            ax.set_ylabel('$g(w)$',fontsize = 25,rotation = 0,labelpad = 25)
+            ax.set_xlabel('$w$',fontsize = 13)
+            ax.set_ylabel('$g(w)$',fontsize = 13,rotation = 0,labelpad = 25)
 
             #### should we plot first order approximation? ####
             if first_order == True:
@@ -114,7 +118,7 @@ class visualizer:
                 h = g_val + g_grad_val*(wrange - w_val)
 
                 # plot the first order approximation
-                ax.plot(wrange,h,color = self.colors[0],linewidth = 3,zorder = 2)      # plot approx
+                ax.plot(wrange,h,color = self.colors[0],linewidth = 2,zorder = 2)      # plot approx
 
             #### should we plot second order approximation? ####
             if second_order == True:
@@ -141,12 +145,19 @@ class visualizer:
                 h = g_val + g_grad_val*(wrange - w_val) + 0.5*g_hess_val*(wrange - w_val)**2 
 
                 # plot the second order approximation
-                ax.plot(wrange,h,color = self.colors[1],linewidth = 3,zorder = 1)      # plot approx
+                ax.plot(wrange,h,color = self.colors[1],linewidth = 2,zorder = 1)      # plot approx
 
             # fix viewing limits on panel
             ax.set_xlim([-max_val,max_val])
             ax.set_ylim([min(g_plot) - ggap,max(g_plot) + ggap])
+            
+            # set tickmarks
+            ax.set_xticks(-np.arange(-round(max_val), round(max_val) + 1, 1.0))
+            ax.set_yticks(np.arange(round(min(g_plot) - ggap), round(max(g_plot) + ggap) + 1, 1.0))
                 
+            # set axis 
+            ax.axhline(y=0, color='k',zorder = 0,linewidth = 0.5)
+            
             return artist,
         
         anim = animation.FuncAnimation(fig, animate,frames=len(w_vals), interval=len(w_vals), blit=True)
