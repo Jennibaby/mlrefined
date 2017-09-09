8fba03eb650f7763b7340e71a7651bad76889392
Jeremy Watt <jermwatt@gmail.com>
2017-09-08 20:09:06 -0500

    test

--- mlrefined_libraries/superlearn_library/lin_regression_demos.py
+++ mlrefined_libraries/superlearn_library/lin_regression_demos.py
@@ -14,6 +14,7 @@ from autograd import hessian as compute_hess
 import math
 import time
 from matplotlib import gridspec
+import copy
 
 class visualizer:
     '''
@@ -22,8 +23,8 @@ class visualizer:
     #### initialize ####
     def __init__(self,data):
         # grab input
-        self.x = data[:,0]
-        self.y = data[:,1]
+        self.x = data[:,:-1]
+        self.y = data[:,-1]
         
     def center_data(self):
         # center data
@@ -50,7 +51,7 @@ class visualizer:
         if 'w_init' in kwargs:
             self.w_init = kwargs['w_init']
             self.w_init = np.asarray([float(s) for s in self.w_init])
-            self.w_init.shape = (2,1)
+            self.w_init.shape = (len(self.w_init),1)
             
         # run algorithm of choice
         if self.algo == 'gradient_descent':
@@ -68,7 +69,10 @@ class visualizer:
     def least_squares(self,w):
         cost = 0
         for p in range(0,len(self.y)):
-            cost +=(w[0] + w[1]*self.x[p] - self.y[p])**2
+            x_p = copy.deepcopy(self.x[p,:])
+            x_p.shape = (len(x_p),1)
+            y_p = self.y[p]
+            cost +=(w[0] + np.dot(w[1:].T,x_p) - y_p)**2
         return cost
                 
     ######## gradient descent ########
@@ -127,9 +131,130 @@ class visualizer:
             # record
             self.w_hist.append(w)
     
-    ######## animation function ########
+     ######## 3d animation function ########
     # animate gradient descent or newton's method
-    def animate_it(self,**kwargs):         
+    def animate_it_3d(self,**kwargs):         
+        ##### setup figure to plot #####
+        # initialize figure
+        fig = plt.figure(figsize = (8,3))
+        artist = fig
+        
+        # create subplot with 3 panels, plot input function in center plot
+        gs = gridspec.GridSpec(1, 2, width_ratios=[2,1]) 
+        ax1 = plt.subplot(gs[0],projection='3d'); 
+        ax2 = plt.subplot(gs[1]);
+
+        # produce color scheme
+        s = np.linspace(0,1,len(self.w_hist[:round(len(self.w_hist)/2)]))
+        s.shape = (len(s),1)
+        t = np.ones(len(self.w_hist[round(len(self.w_hist)/2):]))
+        t.shape = (len(t),1)
+        s = np.vstack((s,t))
+        self.colorspec = []
+        self.colorspec = np.concatenate((s,np.flipud(s)),1)
+        self.colorspec = np.concatenate((self.colorspec,np.zeros((len(s),1))),1)
+        
+        # seed left panel plotting range
+        xmax = 3
+        if 'xmax' in kwargs:
+            wmax = kwargs['xmax']
+        r = np.linspace(-xmax,xmax,200)
+
+        # create grid from plotting range
+        x1_vals,x2_vals = np.meshgrid(r,r)
+        x1_vals.shape = (len(r)**2,1)
+        x2_vals.shape = (len(r)**2,1)
+        
+        x1_vals.shape = (np.size(r),np.size(r))
+        x2_vals.shape = (np.size(r),np.size(r))
+
+        # seed left panel view 
+        view = [20,50]
+        if 'view' in kwargs:
+            view = kwargs['view']
+        
+        # set zaxis to the left
+        self.move_axis_left(ax1)
+            
+        # start animation
+        num_frames = len(self.w_hist)
+        print ('starting animation rendering...')
+        def animate(k):
+            # clear panels
+            ax1.cla()
+            
+            # set axis in left panel
+            self.move_axis_left(ax1)
+            
+            # current color
+            color = self.colorspec[k]
+
+            # print rendering update
+            if np.mod(k+1,25) == 0:
+                print ('rendering animation frame ' + str(k+1) + ' of ' + str(num_frames))
+            if k == num_frames - 1:
+                print ('animation rendering complete!')
+                time.sleep(1.5)
+                clear_output()
+            
+            ###### make left panel - plot data and fit ######
+            # initialize fit
+            w = self.w_hist[k]
+                    
+            # reshape and plot the surface, as well as where the zero-plane is        
+            y_fit = w[0] + w[1]*x1_vals + w[2]*x2_vals
+
+            # plot cost surface
+            ax1.plot_surface(x1_vals,x2_vals,y_fit,alpha = 0.1,color = color,rstride=25, cstride=25,linewidth=1,edgecolor = 'k',zorder = 2)  
+            
+            # scatter data
+            self.scatter_pts(ax1)
+            #ax1.view_init(view[0],view[1])
+            
+            # plot connector between points for visualization purposes
+            if k == 0:
+                w_new = self.w_hist[k]
+                g_new = self.g(w_new)[0][0]
+                ax2.scatter(k,g_new,s = 0.1,color = 'w',linewidth = 2.5,alpha = 0,zorder = 1)      # plot approx
+                
+            if k > 0:
+                w_old = self.w_hist[k-1]
+                w_new = self.w_hist[k]
+                g_old = self.g(w_old)[0][0]
+                g_new = self.g(w_new)[0][0]
+     
+                ax2.plot([k-1,k],[g_old,g_new],color = color,linewidth = 2.5,alpha = 1,zorder = 2)      # plot approx
+                ax2.plot([k-1,k],[g_old,g_new],color = 'k',linewidth = 3.5,alpha = 1,zorder = 1)      # plot approx
+            
+            # set viewing limits for second panel
+            ax2.axhline(y=0, color='k',zorder = 0,linewidth = 0.5)
+            ax2.set_xlabel('iteration',fontsize = 12)
+            ax2.set_ylabel(r'$g(\mathbf{w})$',fontsize = 12,rotation = 0,labelpad = 25)
+            ax2.set_xlim([-0.5,len(self.w_hist)])
+            
+            # set axis in left panel
+            self.move_axis_left(ax1)
+                
+            return artist,
+
+        anim = animation.FuncAnimation(fig, animate ,frames=num_frames, interval=num_frames, blit=True)
+        
+        return(anim)   
+    
+    # set axis in left panel
+    def move_axis_left(self,ax):
+        tmp_planes = ax.zaxis._PLANES 
+        ax.zaxis._PLANES = ( tmp_planes[2], tmp_planes[3], 
+                             tmp_planes[0], tmp_planes[1], 
+                             tmp_planes[4], tmp_planes[5])
+        view_1 = (25, -135)
+        view_2 = (25, -45)
+        init_view = view_2
+        ax.view_init(*init_view)
+    
+    ######## 2d animation function ########
+    # animate gradient descent or newton's method
+    def animate_it_2d(self,**kwargs):         
         ##### setup figure to plot #####
         # initialize figure
         fig = plt.figure(figsize = (8,3))
@@ -223,38 +348,87 @@ class visualizer:
         ax1 = plt.subplot(gs[0]); ax1.axis('off') 
         ax2 = plt.subplot(gs[1]); 
         ax3 = plt.subplot(gs[2]); ax3.axis('off')
+        
+        if np.shape(self.x)[1] == 2:
+            ax2 = plt.subplot(gs[1],projection='3d'); 
 
         # scatter points
         self.scatter_pts(ax2)
         
     # scatter points
     def scatter_pts(self,ax):
-        # set plotting limits
-        xmax = max(self.x)
-        xmin = min(self.x)
-        xgap = (xmax - xmin)*0.2
-        xmin -= xgap
-        xmax += xgap
-        x_fit = np.linspace(xmin,xmax,500)
-        
-        ymax = max(self.y)
-        ymin = min(self.y)
-        ygap = (ymax - ymin)*0.2
-        ymin -= ygap
-        ymax += ygap    
-        
-        # initialize points
-        ax.scatter(self.x,self.y,color = 'k', edgecolor = 'w',linewidth = 0.9,s = 40)
+        if np.shape(self.x)[1] == 1:
+            # set plotting limits
+            xmax = copy.deepcopy(max(self.x))
+            xmin = copy.deepcopy(min(self.x))
+            xgap = (xmax - xmin)*0.2
+            xmin -= xgap
+            xmax += xgap
+            
+            ymax = max(self.y)
+            ymin = min(self.y)
+            ygap = (ymax - ymin)*0.2
+            ymin -= ygap
+            ymax += ygap    
 
-        # clean up panel
-        ax.set_xlim([xmin,xmax])
-        ax.set_ylim([ymin,ymax])
-        
-        # label axes
-        ax.set_xlabel(r'$x$', fontsize = 12)
-        ax.set_ylabel(r'$y$', rotation = 0,fontsize = 12)
-        ax.set_title('data', fontsize = 13)
-        
+            # initialize points
+            ax.scatter(self.x,self.y,color = 'k', edgecolor = 'w',linewidth = 0.9,s = 40)
+
+            # clean up panel
+            ax.set_xlim([xmin,xmax])
+            ax.set_ylim([ymin,ymax])
+
+            # label axes
+            ax.set_xlabel(r'$x$', fontsize = 12)
+            ax.set_ylabel(r'$y$', rotation = 0,fontsize = 12)
+            ax.set_title('data', fontsize = 13)
+            
+        if np.shape(self.x)[1] == 2:
+            # set plotting limits
+            xmax1 = copy.deepcopy(max(self.x[:,0]))
+            xmin1 = copy.deepcopy(min(self.x[:,0]))
+            xgap1 = (xmax1 - xmin1)*0.3
+            xmin1 -= xgap1
+            xmax1 += xgap1
+            
+            xmax2 = copy.deepcopy(max(self.x[:,0]))
+            xmin2 = copy.deepcopy(min(self.x[:,0]))
+            xgap2 = (xmax2 - xmin2)*0.3
+            xmin2 -= xgap2
+            xmax2 += xgap2
+            
+            ymax = max(self.y)
+            ymin = min(self.y)
+            ygap = (ymax - ymin)*0.2
+            ymin -= ygap
+            ymax += ygap    
+
+            # initialize points
+            ax.scatter(self.x[:,0],self.x[:,1],self.y,s = 40,color = 'k', edgecolor = 'w',linewidth = 0.9)
+
+            # clean up panel
+            ax.set_xlim([xmin1,xmax1])
+            ax.set_ylim([xmin2,xmax2])
+            ax.set_zlim([ymin,ymax])
+
+            # label axes
+            ax.set_xlabel(r'$x_1$', fontsize = 12)
+            ax.set_ylabel(r'$x_2$', rotation = 0,fontsize = 12)
+            ax.set_zlabel(r'$y$', rotation = 0,fontsize = 12,labelpad = -3)
+
+            # clean up panel
+            ax.xaxis.pane.fill = False
+            ax.yaxis.pane.fill = False
+            ax.zaxis.pane.fill = False
+
+            ax.xaxis.pane.set_edgecolor('white')
+            ax.yaxis.pane.set_edgecolor('white')
+            ax.zaxis.pane.set_edgecolor('white')
+
+            ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+            ax.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+            ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+           
     # plot points on contour
     def plot_pts_on_contour(self,ax,j,color):
         # plot connector between points for visualization purposes
